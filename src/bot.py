16369109dde438450adc24b58ca6ebf7234a5616
Eduardo Sanchez <egsanche97@gmail.com>
2020-06-30 15:05:48 -0500

    shortened duplicate code

--- src/bot.py
+++ src/bot.py
@@ -8,6 +8,7 @@ import math
 from dotenv import load_dotenv
 from discord.ext import commands
 from datetime import datetime, timedelta
+from discord.utils import get
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -38,7 +39,7 @@ async def help(ctx):
   embed.add_field(name="Example Input", value=".lookup na noodlz kindred ezreal", inline=False)
   await ctx.send(embed=embed)
 
-@client.command(name='lookup')
+@client.command(pass_context=True)
 async def lookup(ctx, arg1, arg2, arg3, arg4):
   """
   Attributes
@@ -63,41 +64,56 @@ async def lookup(ctx, arg1, arg2, arg3, arg4):
   ago : str
     formatted string saying how long ago the game was created, rounds to days/months/years
   """
-  regions = {"br": "br1", "eun":"eun1", "euw":"euw1", "jp":"jp1", "kr":"kr", "la1":"la1", "la2":"la2", "na":"na1", "oc":"oc1", "ru":"ru", "tr":"tr1"}
+  regions = {"br": "br1", "eun":"eun1", "euw":"euw1", "jp":"jp1", "kr":"kr", "la1":"la1", "la2":"la2", "na":"na1", "oce":"oc1", "ru":"ru", "tr":"tr1"}
+  servers = {"br1": "br", "eun1": "eune", "euw1": "euw", "la1": "lan", "la2": "las", "na1": "na", "oce1": "oce", "ru": "ru", "jp1": "jp", "kr": "kr", "tr1": "tr"}
+
   region = regions.get(arg1)
   username = arg2.capitalize()
   champion = arg3.capitalize()
   enemy_champion = arg4.capitalize()
 
   url = f'http://127.0.0.1:5000/{region}/username={username}&champion={champion}&enemy_champion={enemy_champion}'
+  print(url)
   print('Invoked lookup() command, searching for {username} matches. Match up {champion} vs {enemyChamp}'.format(username=arg1, champion=arg2, enemyChamp=arg3))
   response = requests.get(url).json()
 
-  for match_id in response:
+  # creating list of embed objects containing the matches
+  for page, match_id in enumerate(response, 1):
     now = datetime.fromtimestamp(time.time())
+    date_time = datetime.fromtimestamp(response[match_id]['date']).strftime('%Y-%m-%d')
     date = datetime.fromtimestamp(response[match_id]['date'])
+    server = servers.get(region)
+    game_id = response[match_id]['gameId']
+    game_version = response[match_id]['gameVersion']
+    url2 = f'https://matchhistory.{server}.leagueoflegends.com/en/#match-details/{region.upper()}/{game_id}?tab=overview'
+
     difference = now - date
     days = difference.days
     ago = ''
 
-    champion_username = response[match_id][champion]['username']
-    enemy_champion_username = response[match_id][enemy_champion]['username']
+    response_champion = response[match_id][champion]
+    response_enemy_champion = response[match_id][enemy_champion]
 
-    champion_kda = f"{response[match_id][champion]['kills']}/{response[match_id][champion]['deaths']}/{response[match_id][champion]['assists']}"
-    enemy_champion_kda = f"{response[match_id][enemy_champion]['kills']}/{response[match_id][enemy_champion]['deaths']}/{response[match_id][champion]['assists']}"
+    username = response_champion['username']
+    enemy_username = response_enemy_champion['username']
 
-    champion_items = f"{response[match_id][champion]['item0']}, {response[match_id][champion]['item1']}, {response[match_id][champion]['item2']}, {response[match_id][champion]['item3']}, {response[match_id][champion]['item4']}, {response[match_id][champion]['item5']}, {response[match_id][champion]['item6']}"
-    enemy_champion_items = f"{response[match_id][enemy_champion]['item0']}, {response[match_id][enemy_champion]['item1']}, {response[match_id][enemy_champion]['item2']}, {response[match_id][enemy_champion]['item3']}, {response[match_id][enemy_champion]['item4']}, {response[match_id][enemy_champion]['item5']}, {response[match_id][enemy_champion]['item6']}"
+    champion_kda = f"{response_champion['kills']}/{response_champion['deaths']}/{response_champion['assists']}"
+    enemy_champion_kda = f"{response_enemy_champion['kills']}/{response_enemy_champion['deaths']}/{response_champion['assists']}"
 
-    champion_runes = f"{response[match_id][champion]['perk0']}, {response[match_id][champion]['perk1']}, {response[match_id][champion]['perk2']}, {response[match_id][champion]['perk3']}, {response[match_id][champion]['perk4']}, {response[match_id][champion]['perk5']}"
-    enemy_champion_runes = f"{response[match_id][enemy_champion]['perk0']}, {response[match_id][enemy_champion]['perk1']}, {response[match_id][enemy_champion]['perk2']}, {response[match_id][enemy_champion]['perk3']}, {response[match_id][enemy_champion]['perk4']}, {response[match_id][enemy_champion]['perk5']}"
+    champion_items = f"{response_champion['item0']}, {response_champion['item1']}, {response_champion['item2']}, {response_champion['item3']}, {response_champion['item4']}, {response_champion['item5']}, {response_champion['item6']}"
+    enemy_champion_items = f"{response_enemy_champion['item0']}, {response_enemy_champion['item1']}, {response_enemy_champion['item2']}, {response_enemy_champion['item3']}, {response_enemy_champion['item4']}, {response_enemy_champion['item5']}, {response_enemy_champion['item6']}"
+
+    champion_runes = f"{response_champion['perk0']}, {response_champion['perk1']}, {response_champion['perk2']}, {response_champion['perk3']}, {response_champion['perk4']}, {response_champion['perk5']}"
+    enemy_champion_runes = f"{response_enemy_champion['perk0']}, {response_enemy_champion['perk1']}, {response_enemy_champion['perk2']}, {response_enemy_champion['perk3']}, {response_enemy_champion['perk4']}, {response_enemy_champion['perk5']}"
     
-    champion_perks = f"{response[match_id][champion]['statPerk0']}, {response[match_id][champion]['statPerk1']}, {response[match_id][champion]['statPerk2']}"
-    enemy_champion_perks = f"{response[match_id][enemy_champion]['statPerk0']}, {response[match_id][enemy_champion]['statPerk1']}, {response[match_id][enemy_champion]['statPerk2']}"
+    champion_perks = f"{response_champion['statPerk0'] or 'None'}, {response_champion['statPerk1'] or 'None'}, {response_champion['statPerk2'] or 'None'}"
+    enemy_champion_perks = f"{response_enemy_champion['statPerk0'] or 'None'}, {response_enemy_champion['statPerk1'] or 'None'}, {response_enemy_champion['statPerk2'] or 'None'}"
 
-    champion_spells = f"{response[match_id][champion]['spell1']}, {response[match_id][champion]['spell2']}"
-    enemy_champion_spells = f"{response[match_id][enemy_champion]['spell1']}, {response[match_id][enemy_champion]['spell2']}"
+    champion_spells = f"{response_champion['spell1']}, {response_champion['spell2']}"
+    enemy_champion_spells = f"{response_enemy_champion['spell1']}, {response_enemy_champion['spell2']}"
     
+    game_version_split = game_version.split('.')
+    patch = f'{game_version_split[0]}.{game_version_split[1]}'
     if days / 30.4167 < 1:
       ago = str(days) + ' days ago'
     if days / 30.4167 > 1 and days / 30.4167 < 11.90:
@@ -109,10 +125,10 @@ async def lookup(ctx, arg1, arg2, arg3, arg4):
 
     embed = discord.Embed (
     title = ago,
-    description = f'Match details... {champion} ({champion_username}) vs {enemy_champion} ({enemy_champion_username})',
-    colour = discord.Colour.blue() if response[match_id][champion]['win'] == True else discord.Colour.red()
+    url=url2,
+    description = f'Patch: {patch}\nDate: {date_time}\nMatch Up: {champion} ({username}) vs {enemy_champion} ({enemy_username})',
+    colour = discord.Colour.blue() if response_champion['win'] == True else discord.Colour.red()
     )
-
     embed.add_field(name="Champion", value=champion, inline=True)
     embed.add_field(name="K/D/A", value=champion_kda, inline=True)
     embed.add_field(name="Summoner Spells", value=champion_spells, inline=True)
@@ -126,8 +142,11 @@ async def lookup(ctx, arg1, arg2, arg3, arg4):
     embed.add_field(name="Items", value=enemy_champion_items, inline=True)
     embed.add_field(name="Runes", value=enemy_champion_runes, inline=True)
     embed.add_field(name="Rune Perks", value=enemy_champion_perks, inline=True)
-
     await ctx.send(embed=embed)
+    print(page)
+
+  # creating page listing with forwards and backwards functionality
+
 
 @client.event
 async def on_command_error(ctx, error):
